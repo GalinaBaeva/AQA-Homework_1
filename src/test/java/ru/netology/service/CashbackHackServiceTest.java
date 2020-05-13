69070b5ba2462aa3cba189db0c656f380313eab2
GalinaBaeva <baeva_galina@mail.com>
2020-05-13 22:05:14 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn0IfAmount1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn499IfAmount501() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 501;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 499;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn1000IfAmount0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+}
